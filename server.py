73967430b167caea6d95938096b405967bb4fc11
David Parise <parise@ualberta.ca>
2022-09-29 22:55:39 -0600

    Added identifier

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from genericpath import isfile
 import socketserver
 import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 David Parise
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
